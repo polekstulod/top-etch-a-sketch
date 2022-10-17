02de40268a092fad367dd92a669e3f92ad49fbbb
James Paul Tulod <paultulod@pm.me>
2022-10-17 11:56:59 +0800

    Add setColor function

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ for (let i = 0; i < 256; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
+  square.addEventListener('mouseover', () => setColor(square))
+
   container.appendChild(square)
+}
+
+function setColor(element) {
+  const color = '#f8f8f2'
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
