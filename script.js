1ad513a38db669a4026a2af28f4d916a1ba92abd
James Paul Tulod <paultulod@pm.me>
2022-10-17 11:32:52 +0800

    Create multiple divs

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container')
+
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  container.appendChild(square)
+}
